d0979ceadcea0bde9cf0f47e0dd157e863f21bc4
Siddharth Pathak <siddharthpathak415@gmail.com>
2024-02-25 16:03:36 -0800

    created a file to get live match data via api

---
+++ frontend/src/services/matchService.ts
@@ -0,0 +1,25 @@
+// src/services/matchService.ts
+import axios from 'axios';
+
+interface MatchData {
+  // Define the structure of the match data you expect
+  // This should mirror what your backend sends, which might be a subset of PandaScore's data
+  id: number;
+  name: string;
+  game: string;
+
+}
+
+const url = process.env.BASE_URL; 
+
+const fetchLiveMatches = async (): Promise<MatchData[]> => {
+  try {
+    const response = await axios.get(`${url}/livematches`);
+    return response.data; // Assuming the backend sends the data in the response body directly
+  } catch (error) {
+    console.error("Error fetching live matches:", error);
+    throw new Error('Failed to fetch live matches');
+  }
+};
+
+export { fetchLiveMatches };
